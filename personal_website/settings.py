a83875902900fdc947096e4273d98db6642eced7
denin16 <navdeep.beniwal1997@gmail.com>
2018-06-06 10:29:26 +0530

    Added postgres database to project

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'personal_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'personal_website',
+        'USER':'postgres',
+        'PASSWORD':'Denin16@vit',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
