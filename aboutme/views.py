132fb351b4957c6cf5b5a738e882d33b09bf2aa2
denin16 <navdeep.beniwal1997@gmail.com>
2018-06-08 09:33:16 +0530

    aboutme page done

--- aboutme/views.py
+++ aboutme/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Aboutme
 # Create your views here.
+def aboutme(request):
+    descriptions = Aboutme.objects
+    return render(request,'aboutme/aboutmepage.html',{'descriptions':descriptions})
--- personal_website/urls.py
+++ personal_website/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import aboutme.views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',aboutme.views.aboutme,name='aboutme'),
 ]
